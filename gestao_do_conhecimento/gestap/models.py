ee89a387967981fdd424afbb371cd1b237adf47b
Raphael <raphaelsantos.medi@gmail.com>
2023-06-30 18:58:54 -0300

    Finalizando o model Funcionario

--- gestao_do_conhecimento/gestap/models.py
+++ gestao_do_conhecimento/gestap/models.py
@@ -4,4 +4,13 @@ from django.db import models
 
 class Funcionario(models.Model):
    nome = models.CharField(max_length=100)
-   cargo = models.CharField(max_length=)
+   cargo = models.CharField(max_length=20)
+   email = models.CharField(max_length=100)
+   departamento = models.CharField(max_length=20)
+   data_nascimento = models.DateField()
+   data_admissao = models.DateField()
+   salario = models.DecimalField(max_digits=10, decimal_places=2)
+   competências = models.TextField()
+   supervisor = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+   foto = models.ImageField(upload_to='funcionarios/', null=True, blank=True) 
+
