446c7177e6c90c873e6085dde11fe1d20771195d
Raphael <raphaelsantos.medi@gmail.com>
2023-07-01 02:46:04 -0300

    views create

--- gestao_do_conhecimento/gestap/views.py
+++ gestao_do_conhecimento/gestap/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
+from .models import Funcionario
+from .forms import Funcionarioform
+import datetime
 
 # Create your views here.
+
+""" def home(request):
+    data = {}
+    lista = []
+
+    for c in range(10):
+        lista.append(c+1)
+
+    data['transacoes'] = [lista]
+    data['now'] = datetime.datetime.now()
+    return render(request, 'gestap/home.html') """
+
+def listagem(request):
+    funcionarios = Funcionario.objects.all()
+
+    return render(request, 'gestap/listagem.html', {'funcionarios': funcionarios})
+
 
